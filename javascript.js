ff57528b83353f250fc0c809190aecaa6d30b965
Cody Black <cdblack86@gmail.com>
2022-08-24 11:04:49 -0400

    Variables assigned and user input case insensitive
    Variables have been assigned to the computer choice and the player
    input, also the player input has been made case insensitive

--- javascript.js
+++ javascript.js
@@ -3,13 +3,16 @@ function getComputerChoice () {
     let weapon = ["rock", "paper", "scissors"]
     return weapon[Math.floor(Math.random() * 3)]
 }
-console.log (getComputerChoice())
-
-
+let comChoice = getComputerChoice()
+console.log (comChoice)
 
 //Ask for user input of rock, paper, or scissors
 let userInput = prompt("Please select rock, paper, or scissors: ")
+let uInput = userInput.toLowerCase()
+console.log (uInput)
 //compare input values
+
 //If user matches computer output tie 
+
 //If user beats computer output you win 
-//If computer beats user output you lose 
+//If computer beats user output you lose
