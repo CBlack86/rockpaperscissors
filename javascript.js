707ced4a2665dfd6687c454ef7abf0f3c118e9ad
Cody Black <cdblack86@gmail.com>
2022-08-25 10:28:28 -0400

    Reworked game function to play multiple rounds
    Reworked the play function so that the game could be played multiple
    times then added a function to repeat play until someone had five wins

--- javascript.js
+++ javascript.js
@@ -1,78 +1,113 @@
+//starting scores for both sides
 userScore = 0
 comScore = 0
-let playerWinRound = "You win this round!"
-let computerWinRound = "Computer wins this round!"
-let tie = "You have tied!"
-let playerWin = "You have won the game! Congrats!"
-let computerWin = "The computer has bested you, better luck next time"
 
 // Have computer choose a random number from 0-2
 function getComputerChoice () {
     let weapon = ["rock", "paper", "scissors"]
     return weapon[Math.floor(Math.random() * 3)]
 }
-let comChoice = getComputerChoice()
-console.log (comChoice)
 
-//Ask for user input of rock, paper, or scissors
-let userInput = prompt("Please select rock, paper, or scissors: ")
-let uInput = userInput.toLowerCase()
-console.log (uInput)
+
+
 //compare input values
 
-function playRound(comChoice, uInput) {
-   if (comChoice === uInput) {
-    return tie
-   } 
-   else if (uInput === "rock" && comChoice === "scissors") {
-    userScore++
-    return playerWinRound
-    }
-   else if (uInput === "paper" && comChoice === "rock") {
-    userScore++
-    return playerWinRound
-    }
-   else if (uInput === "scissors" && comChoice === "paper") {
-    userScore++
-    return playerWinRound
-    }
-   else {
-    comScore++
-    return computerWinRound
-    }
+function playRound() {
+//Ask for user input of rock, paper, or scissors
+    let userInput = prompt("Please select rock, paper, or scissors: ").toLowerCase();
+    let comChoice = getComputerChoice();
+  if (userInput ==  comChoice) {
+      let result = "Tie! Try Again!"
+      result += '\n'
+      result += " Your Score: " + userScore
+      result += '\n'
+      result += " Computer Score: " + comScore
+      return result;
+  }
+  
+  if (userInput == 'rock' && comChoice == 'paper') {
+      comScore++;
+      let result = "You Lose! Paper beats rock!"
+      result += '\n'
+      result += " Your Score: " + userScore
+      result += '\n'
+      result += " Computer Score: " + comScore
+      return result;
+  }
+  
+  if (userInput == 'rock' && comChoice == 'scissors') {
+      userScore++;
+      let result = "You Win! Rock beats scissors!"
+      result += '\n'
+      result += " Your Score: " + userScore
+      result += '\n'
+      result += " Computer Score: " + comScore
+      return result;
+
+  }
+  
+  if (userInput == 'paper' && comChoice == 'rock') {
+      userScore++;
+      let result = "You Win! Paper beats rock!"
+      result += '\n'
+      result += " Your Score: " + userScore
+      result += '\n'
+      result += " Computer Score: " + comScore
+      return result;
+  }
+  
+  if (userInput == 'paper' && comChoice == 'scissors') {
+      comScore++;
+      let result = "You Lose! Scissors beats paper!"
+      result += '\n'
+      result += " Your Score: " + userScore
+      result += '\n'
+      result += " Computer Score: " + comScore
+      return result;
+
+  }
+  
+  if (userInput == 'scissors' && comChoice == 'rock') {
+      comScore++;
+      let result = "You Lose! Rock beats scissors!";
+      result += '\n'
+      result += " Your Score: " + userScore
+      result += '\n'
+      result += " Computer Score: " + comScore
+      return result;
+  }
+  
+  if (userInput == 'scissors' && comChoice == 'paper') {
+      userScore++;
+      let result = "You Win! Scissors beats paper!"
+      result += '\n'
+      result += " Your Score: " + userScore
+      result += '\n'
+      result += " Computer Score: " + comScore
+      return result;
+  }
 }
 
-//console.log (playRound(comChoice, uInput))
-//console.log ("Your Score is: " + userScore)
-//console.log ("Computer Score is: " + comScore)
 
-for(var i=0;i<5;i++){
-    let uInput = prompt("Please select rock, paper, or scissors: ");
-    let comChoice = getComputerChoice()
-    console.log (comChoice)
-    console.log (uInput)
-    console.log("your score = " + userScore);
-    console.log("Computer's score = " + comScore);
+function endGame() {
+  if (userScore > comScore) {
+      console.log("Game Over! You Win! :)");
+  } else if (comScore > userScore) {
+      console.log("Game Over! You Lost! :(");
   }
-//function endGame() {
-//    if (userScore > comScore) {
-//        console.log (playerWin)
-//    }
-//    else {
-//        console.log (computerWin)
-//    }
-//}
+}
 
-//function game() {
-//   console.log(playRound());
-//    if (userScore < 3 && comScore < 3) {
-//        game();
-//    }
-//    else {
-//        endGame();
-//    }
-//}
-    
+function game() {
+  console.log(playRound());
+  if(userScore < 5 && comScore < 5){
+    game();
+  }
+  else{
+    endGame();
+  }
+}
 
-//now keep track of wins for computer and user
+
+game();
+   
 
